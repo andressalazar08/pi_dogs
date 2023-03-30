403516918d00fc9b5dbba7ceef75a75d2f1bd242
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-03-30 14:15:42 -0500

    landing page done

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,11 +3,18 @@ import { Link } from "react-router-dom";
 import style from "./NavBar.module.css";
 
 
+const linkStyle = {
+    color:'hsl(215, 51%, 70%)',
+    textDecoration:"none",
+    fontSize:"1.5rem",
+}
+
+
 const NavBarapp = ()=>{
     return(
         <div className={style.mainContainer}>
-                <Link to="/home">Home</Link>
-                <Link to="/create">Create</Link>
+                <Link to="/home" style={linkStyle}>Home</Link>
+                <Link to="/create" style={linkStyle}>Create</Link>
 
         </div>
 
--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,10 +1,81 @@
+import style from "./Landing.module.css";
+import imagedog from "./doggie.jpg";
+import imagereact from "./react.jpg";
+import imageredux from "./redux.jpg";
+import imagepostgresql from "./postgresql.jpg";
+import imagenodejs from "./node.jpg";
+import imageexpress from "./express.jpg";
+// import imagedog from "../../../public/doggie.jpg";
+
 const Landing = () =>{
     return(
-        <>
-            <h1> Esta es la vista de Landing Page</h1>
+        <div>
 
 
-        </>
+            <main className={style.mainContainer}>
+
+
+
+
+                <section className={style.secondContainer}>
+                    <h2 className={style.mainContainerTitle}> Dogs Proyecto Individual</h2>
+
+                    <div className={style.contenedorImagen}>
+                            <img src={imagedog} className={style.mainImage}></img>
+                    </div>
+
+
+                    <div className={style.textoPrincipal}>
+
+
+                    </div>
+
+
+                    <div className={style.datosCabezera}>
+                    {/* <hr className={style.linea}></hr> */}
+
+                        <h3 className={style.datosCabezeraTitle}>Tech stack:</h3>
+
+                        <div className={style.datosCabezeraimgs}>
+                            <img src={imagereact} className={style.techImage1}></img>
+                            <img src={imageredux} className={style.techImage}></img>
+                        </div>
+
+
+                        <div className={style.datosCabezeraimgs2}>
+                            <img src={imagepostgresql} className={style.techImage3}></img>
+                            <img src={imagenodejs} className={style.techImage4}></img>
+                            <img src={imageexpress} className={style.techImage4}></img>
+
+
+                        </div>
+
+
+
+
+                    </div>
+
+
+                </section>
+
+
+
+
+
+            <div className={style.attribution}>
+                <h3>Coded by Felipe Salazar | 2023</h3>
+                <h3><a>Linkedin</a>   |  <a>Github</a></h3>
+
+
+
+
+            </div>
+
+            </main>
+
+
+
+        </div>
     )
 }
 
